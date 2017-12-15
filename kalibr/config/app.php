f71d482a5c3d5b4ab27b0ec3ccdfe7578c8bb068
Marc <msnijdersm@gmail.com>
2017-12-15 13:58:15 +0500

    Install Laravel Zip

--- kalibr/config/app.php
+++ kalibr/config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        ZanySoft\Zip\ZipServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Zip' => ZanySoft\Zip\ZipFacade::class,
     ],
 
 ];
